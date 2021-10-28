c0646fe9ce1cc28451728fd345238d757c6ce372
Gabriel <gcamposlopes@gmail.com>
2021-10-28 08:05:06 -0300

    revisando e inserindo conceitos de fast refresh e imutabilidade

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
 
 // Sempre que uma function começar com 'use', ela eh chamada de -> hook
-
+// Imutabilidade - variavel nunca vai ter seu valor alterado, mas pode
+// ser dado novo valor para mesma.
 export function Counter() {
   // let - let it change
   // const - constant
