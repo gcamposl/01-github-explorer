e650a3c8299eb39ea8a9e3f4138e06a21ff5abc8
Gabriel <gcamposlopes@gmail.com>
2021-10-28 09:18:20 -0300

    entendendo o funcionamento do hook useEffect.

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/global.scss'
 import '../styles/repositories.scss'
+import { useState, useEffect } from "react";
 
 
 const repository = {
@@ -9,7 +10,23 @@ const repository = {
   link: 'https://github.com/unform/unform'
 }
 
+// useEffect(() =>{
+
+// },[repositories]); -> Primeiro parametro: Funçao que executa, quado o segundo,
+//parametro mudar! Caso o segundo parametro seja vazio, executa apenas uma vez e se 
+// não houver um segundo parametro, o useEffect entra em loop!
+
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/gcamposl/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, []);
+
+  console.log(repositories);
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
